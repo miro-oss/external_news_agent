99727ccdbef4a7d7537311ccd29b9dfca9445fba
miro-oss <roxkstar@Roxkstar-2.local>
2026-08-11 13:00:18 +0900

    [FIX/#3] 백엔드 테스트 DB 자동설정 제외

--- BE/src/test/java/com/example/be/BeApplicationTests.java
+++ BE/src/test/java/com/example/be/BeApplicationTests.java
@@ -1,9 +1,16 @@
 package com.example.be;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        FlywayAutoConfiguration.class
+})
 class BeApplicationTests {
 
     @Test
